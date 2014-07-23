1383f55e8550cab9786b7253d849e3fa719ccaf4
Chris Antaki <ChrisAntaki@gmail.com>
2014-07-23 13:25:31 -0700

    Genesis

---
+++ jquery.template.js
@@ -0,0 +1,28 @@
+/*!
+* @license Copyright 2014 Chris Antaki. MIT License: http://en.wikipedia.org/wiki/MIT_License
+*/
+
+;(function ($) {
+    var templates = {};
+
+    function jQueryDotTemplate (target, values) {
+        if (!templates[target]) {
+            templates[target] = $(target).html();
+        }
+
+        var html = templates[target];
+        for (var i in values) {
+            var pattern = new RegExp('\\$' + i, 'g');
+            html = html.replace(pattern, values[i]);
+        }
+
+        var $html = $(html);
+        for (var i in values) {
+            $html.find('.text-' + i).text(values[i]);
+        }
+
+        return $html;
+    }
+
+    $.template = jQueryDotTemplate;
+})(jQuery);
