7277dd61fe5319375c03ca3499d8232f2e2be8fd
Chris Antaki <ChrisAntaki@gmail.com>
2014-07-27 00:22:18 -0700

    Added class- directive

--- jquery.template.js
+++ jquery.template.js
@@ -19,6 +19,7 @@
         var $html = $(html);
         for (var i in values) {
             $html.find('.attribute-' + i).attr(i, values[i]);
+            $html.find('.class-' + i).addClass(values[i]);
             $html.find('.href-' + i).attr('href', values[i]);
             $html.find('.src-' + i).attr('src', values[i]);
             $html.find('.target-' + i).attr('target', values[i]);
