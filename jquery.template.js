8d70ccaca3772f8752dc897b4ba2334b91143fa0
Chris Antaki <ChrisAntaki@gmail.com>
2014-07-27 00:16:56 -0700

    Update jquery.template.js

--- jquery.template.js
+++ jquery.template.js
@@ -19,6 +19,9 @@
         var $html = $(html);
         for (var i in values) {
             $html.find('.attribute-' + i).attr(i, values[i]);
+            $html.find('.href-' + i).attr('href', values[i]);
+            $html.find('.src-' + i).attr('src', values[i]);
+            $html.find('.target-' + i).attr('target', values[i]);
             $html.find('.text-' + i).text(values[i]);
         }
 
